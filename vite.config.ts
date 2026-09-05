6386534d6a2dba576273b24b902035a3b8a8d17d
sravanTIRU <sravan1482000@gmail.com>
2026-09-05 15:03:54 +0530

    Fix repo link

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
@@ -7,4 +7,5 @@ export default defineConfig({
     port: 3000,
     open: false,
   },
+  base: "SravanTG",
 });
